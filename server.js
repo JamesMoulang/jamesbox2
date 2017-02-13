eb06d468d6bbd31902424e5290422d887e0426fb
James Moulang <jamesmoulang123@gmail.com>
2017-02-13 23:16:41 +0000

    port

--- server.js
+++ server.js
@@ -124,6 +124,6 @@ io.on('connection', function(socket) {
 		}
 	});
 });
-server.listen(3000, function() {
+server.listen(process.env.PORT || 3000, function() {
 	console.log("listening on port 3000");
 });
